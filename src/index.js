668bd3e37fde1a67f6c5cf171eabfcae31dc9223
RPG0007 <90514460+RPG0007@users.noreply.github.com>
2022-07-23 12:40:17 +0300

    docs:solved

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    let tosort = []
+    if (Array.isArray(matrix) && matrix.length >0){
+     tosort = matrix.reduce((acc, cur, i) =>{
+      if (i%2==1 && i!=0){
+       acc.push(...cur.sort((a,b)=>b-a))}
+       else{
+        acc.push(...cur) 
+       }
+       return acc
+     })
+     return tosort
+    }
+    else
+    {
+        return []
+    }
+      
+ }
